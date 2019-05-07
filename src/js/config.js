27f967e8097fac0f85d433577fef55d00ca4bd03
vlvlwang <13730628501@.com>
2019-05-07 21:06:05 +0800

    静态页登录

--- src/js/config.js
+++ src/js/config.js
@@ -3,6 +3,6 @@ require.config({
     paths : {
         "jquery" : "libs/jquery/jquery-3.2.1",
         "header" : "js/module/header",
-        "footer" : "js/module/footer"
+        "footer" : "js/module/footer",
     }
 })
